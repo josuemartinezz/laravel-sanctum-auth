81a37063f423c4fccacbf23afa148f1a951677a8
Josué Ayala <ayalavalencia11@gmail.com>
2021-07-16 16:39:34 -0600

    👀 Change method /user route

--- routes/api.php
+++ routes/api.php
@@ -23,5 +23,5 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/user', [AuthController::class, 'userInfo'])->middleware('auth:sanctum');
+    Route::get('/user', [AuthController::class, 'userInfo'])->middleware('auth:sanctum');
 });
